d13af4204de3c4130eac270430c3975c95928923
Hari <hari@javahome.in>
2017-06-28 17:02:19 +0530

    fixed a defect

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I'm running in a docker container & I have been seen {} times.\n'.format(count)
+    return 'Hello World! Im running in a docker container & I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
